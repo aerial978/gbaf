48b2179ebdfb703bd29310e68fb340b28654c19c
Michel Hathier <mhathiergmail.com>
2022-09-12 21:02:47 +0200

    creation database + inscription

---
+++ actions/signupAction.php
@@ -0,0 +1,40 @@
+<?php
+require('actions/database.php');
+
+if (isset($_POST['submit'])) {
+    if (!empty($_POST['username']) && !empty($_POST['firstname']) && !empty($_POST['lastname']) && !empty($_POST['question']) && !empty($_POST['answer']) && !empty($_POST['password'] && !empty($_POST['confirmpassword']))) {
+        $username = htmlspecialchars($_POST['username']);
+        $firstname = htmlspecialchars($_POST['firstname']);
+        $lastname = htmlspecialchars($_POST['lastname']);
+        $question = htmlspecialchars($_POST['question']);
+        $answer = password_hash($_POST['answer'], PASSWORD_DEFAULT);
+        $password = htmlspecialchars($_POST['password']);
+        $confirmPassword = htmlspecialchars($_POST['confirmpassword']);
+
+        $checkUser = $bdd->prepare('SELECT username FROM user WHERE username = ?');
+        $checkUser->execute(array($username));
+
+        if ($checkUser->rowCount() == 0) {
+            if ($password == $confirmPassword) {
+                $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $insertUser = $bdd->prepare('INSERT INTO user (username, first_name, last_name, question, answer, password) VALUES(?, ?, ?, ?, ?, ?)');
+                $insertUser->execute(array($username,$firstname,$lastname,$question,$answer,$password));
+
+                $getUser = $bdd->prepare('SELECT * FROM user WHERE first_name = ? AND last_name = ?');
+                $getUser->execute(array($firstname, $lastname));
+
+                $_SESSION['auth'] = $getUser->fetch()['id'];
+                $_SESSION['firstname'] = $firstname;
+                $_SESSION['lastname'] = $lastname;
+
+                header('Location: home.php');
+            } else {
+                $errorMsg = "Invalid password !";
+            }
+        } else {
+            $errorMsg = "User already used !";
+        }
+    } else {
+        $errorMsg = "Please complete all fields !";
+    }
+}
---
+++ signup.php
@@ -0,0 +1,74 @@
+<?php
+require('actions/signupAction.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="assets/images/logo-gbaf.png" />
+    <title>Sign up - GBAF</title>
+    <link rel="stylesheet" href="assets/css/formStyle.css">
+    <link rel="stylesheet" href="assets/css/msgStyle.css">
+</head>
+<body>
+    <div class="top-band"></div>
+        <div class="container">
+            <div class="brand">
+                <div class="logo">
+                    <img src="assets/images/logo-gbaf.png" alt="logo" />
+                </div>
+                <div class="logo-title">
+                    <h5>Groupement Bancaire-Assurance Francais</h5>
+                </div>
+            </div>
+            <div class="form">
+                <h1 class="form-title">Sign up</h1>
+                
+                <?php include 'includes/messageFlash.php'; ?>
+                
+                <form class="sign-form" method="POST">
+                    <div class="field">
+                        <label for="username">Username</label>
+                        <input class="input" type="text" name="username">
+                    </div>
+                    <div class="field">
+                        <label for="firstname">First name</label>
+                        <input class="input" type="text" name="firstname">
+                    </div>
+                    <div class="field">
+                        <label for="lastname">Last name</label>
+                        <input class="input" type="text" name="lastname">
+                    </div>
+                    <div class="field">
+                        <label for="question">Secret Question</label>
+                        <input class="input" type="text" name="question">
+                    </div>
+                    <div class="field">
+                        <label for="answer">Secret Answer</label>
+                        <input class="input" type="text" name="answer">
+                    </div>
+                    <div class="field">
+                        <label for="password">Password</label>
+                        <input class="input" type="password" name="password">
+                    </div>
+                    <div class="field">
+                        <label for="password">Confirm Password</label>
+                        <input class="input" type="password" name="confirmpassword">
+                    </div>
+                    <button class="btn" type="submit" name="submit">Submit</button>
+                    <div class="form-link">
+                        <p>Already an account ? <a href="inscription.php">Sign in !</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    <div class="bottom-band"></div>
+</body>
+</html>
+
+<style>
+    
+</style>
