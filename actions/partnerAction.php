b44adcd84b31b23e9e1141977a4167c627d539d9
Michel Hathier <mhathiergmail.com>
2023-02-27 16:14:12 +0100

    Creation partner page, like/dislike & comments systems

---
+++ actions/partnerAction.php
@@ -0,0 +1,100 @@
+<?php
+require('database.php');
+// PARTNER DATA
+$req = $bdd->prepare('SELECT * FROM partner WHERE id = ?');
+$req->execute(array($_GET['partner']));
+$partner = $req->fetch();
+
+// LIKE DISLIKE PROCESSING
+$id = $partner['id'];
+// Nombre de like du partner //
+$likes = $bdd->prepare('SELECT id FROM likes WHERE id_partner = ?');
+$likes->execute([$id]);
+$likes = $likes->rowCount();
+
+// Nombre de dislike du partner //
+$dislikes = $bdd->prepare('SELECT id FROM dislikes WHERE id_partner = ?');
+$dislikes->execute([$id]);
+$dislikes = $dislikes->rowCount();
+
+if (isset($_GET['likedislike']) AND isset($_GET['id']) AND !empty($_GET['likedislike']) AND !empty($_GET['id'])) {
+    $getId = (int) $_GET['id'];
+    $getLikeDislike = (int) $_GET['likedislike'];
+    // Contrôle si le partner existe
+    $checkPartner = $bdd->prepare('SELECT id FROM partner WHERE id = ?');
+    $checkPartner->execute([$getId]);
+    // Si le partner existe
+    if ($checkPartner->rowCount() > 0) {
+        // Si le vote est à like, on récupère le like de l'utilisateur attribué au partner
+        if ($getLikeDislike == 1) {
+            $checkLike = $bdd->prepare('SELECT id FROM likes WHERE id_partner = ? AND id_user = ?');
+            $checkLike->execute([$getId,$_SESSION['id']]);
+            // On change de choix, on retire simultanément le dislike pour ajouter le like
+            $delete = $bdd->prepare('DELETE FROM dislikes WHERE id_partner = ? AND id_user = ?');
+            $delete->execute([$getId,$_SESSION['id']]);
+            // Si l'utilisateur à déjà voter like pour ce partner et qu'il clique à nouveau sur like, on retire le like
+            if ($checkLike->rowcount() == 1) {
+                $delete = $bdd->prepare('DELETE FROM likes WHERE id_partner = ? AND id_user = ?');
+                $delete->execute([$getId,$_SESSION['id']]);
+            } else {
+                // Sinon l'utilisateur n'a pas encore voté like pour ce partner et qu'il clique sur like, on ajoute le like
+                $insert = $bdd->prepare('INSERT INTO likes (id_partner,id_user) VALUES (?,?)');
+                $insert->execute([$getId,$_SESSION['id']]);
+            }
+        // Si le vote est à dislike, on récupère le dislike de l'utilisateur attribué au partner
+        } else {
+            $checkLike = $bdd->prepare('SELECT id FROM dislikes WHERE id_partner = ? AND id_user = ?');
+            $checkLike->execute([$getId,$_SESSION['id']]);
+            // On change de choix, on retire simultanément le like pour ajouter le dislike
+            $delete = $bdd->prepare('DELETE FROM likes WHERE id_partner = ? AND id_user = ?');
+            $delete->execute([$getId,$_SESSION['id']]);
+            // Si l'utilisateur à dèjà voter dislike pour ce partner et qu'il clique à nouveau sur dislike, on retire le dislike
+            if ($checkLike->rowcount() == 1) {
+                $delete = $bdd->prepare('DELETE FROM dislikes WHERE id_partner = ? AND id_user = ?');
+                $delete->execute([$getId,$_SESSION['id']]);
+            } else {
+                // Sinon l'utilisateur n'a pas encore voté dislike pour ce partner et qu'il clique sur dislike, on ajoute le dislike
+                $insert = $bdd->prepare('INSERT INTO dislikes (id_partner,id_user) VALUES (?,?)');
+                $insert->execute([$getId,$_SESSION['id']]);
+            }
+        }
+    }
+    header('Location: ../partner.php?partner='.$getId);
+}
+
+// COUNT COMMENTS
+$req = $bdd->query('SELECT COUNT(*) AS total FROM comment LEFT JOIN partner ON comment.id_partner = partner.id WHERE id_partner = '.$_GET['partner'].''); 
+$countCommentsPartners = $req->fetchAll();
+
+// IF USER COMMENT ALREADY EXISTS
+$req= $bdd->prepare('SELECT * FROM comment 
+LEFT JOIN user ON comment.id_user = user.id 
+LEFT JOIN partner ON comment.id_partner = partner.id
+WHERE user.id = ? AND partner.id = ?');
+$req->execute([$_SESSION['id'],$_GET['partner']]);
+$verifComment = $req->fetchAll();
+
+// ADD COMMENT
+if (isset($_POST['submit'])) {
+    if (!empty($_POST['texte'])) {
+        $texte = htmlspecialchars($_POST['texte']);
+
+        $insertComment = $bdd->prepare('INSERT INTO comment (id_partner, id_user, created_at, texte) VALUES (:id_partner, :id_user, NOW(), :texte)');
+        $insertComment->execute([
+            'id_partner' => $partner['id'],
+            'id_user' => $_SESSION['auth']['id'],
+            'texte' => $texte
+        ]);
+        header('Location: '.$_SERVER['REQUEST_URI'].'');
+    } else {
+        $errorMsg = "Veuillez saisir un commentaire !";
+    }
+}
+
+// COMMENTS LIST
+$getCommentsLists = $bdd->prepare('SELECT last_name, DATE_FORMAT(created_at, \'%d/%m/%Y à %Hh%imin%ss\') AS created_at_fr, texte FROM comment 
+LEFT JOIN user ON comment.id_user = user.id
+WHERE id_partner = ? ORDER BY created_at DESC');
+$getCommentsLists->execute([$partner['id']]);
+
+
---
+++ partner.php
@@ -0,0 +1,154 @@
+<?php
+require('actions/partnerAction.php');
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../assets/css/partnerStyle.css">
+    <link rel="stylesheet" href="../assets/css/commentStyle.css">
+    <link rel="stylesheet" href="assets/css/msgStyle.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="container">
+                <div class="logo">
+                    <a href="home.php"><img src="../assets/images/logo-gbaf.png" alt="logo"></a>
+                </div>
+                <div class="user-session">
+                    <?php if (isset($_SESSION['id'])) : ?>
+                        <div class="avatar-user">  
+                            <a href="account_user.php?id=<?= $_SESSION['id']; ?>"><img src="../assets/images/user-default.png" alt="avatar"></a>
+                        </div>
+                    <?php endif; ?>
+                    <?php if (isset($_SESSION['auth'])) : ?>
+                        <div class="username">
+                            <p><?= $_SESSION['firstname'] . ' ' . $_SESSION['lastname']; ?></p>
+                        </div>
+                    <?php endif; ?>
+                    <a class="rounded-button" href="actions/logoutAction.php"><i class="fas fa-sign-out-alt"></i></a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div class="container">
+        <section class="partner">
+            <img src="assets/images/partners/<?= $partner['logo']; ?>" alt="logo-partner" width="75%">
+        </section>
+    </div>
+    <section class="block container">
+        <div class="block-about">
+            <div class="block-left">
+                <div class="title-left">
+                    <h5>Acteur / partenaire</h5>
+                    <h2><?= $partner['name'] ?></h2>
+                </div>
+                <div class="content-left">
+                    <p><?= $partner['outline'] ?></p>
+                    <p><?= $partner['content'] ?></p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Comments -->
+    <div class="container-comment">
+        <section class="comment-block">
+            <div class="top">
+                <?php foreach($countCommentsPartners as $countCommentsPartner): ?>
+                    <h3 class="comment-number"><?= $countCommentsPartner['total']; ?> commentaire(s)</h3>
+                <?php endforeach; ?>
+                <?php if(empty($verifComment)) : ?>
+                    <button id="show" onclick="show()" class="comment-button">Nouveau commentaire</button>
+                <?php else : ?>
+                <p>déjà commenté !</p>
+                <?php endif; ?>
+                <!-- Like dislike buttons -->
+                <div class="likedislike">
+                    <div class="like">
+                        <a href="actions/partnerAction.php?likedislike=1&id=<?= $_GET['partner'] ?>"><i class="fa fa-thumbs-up fa-xl"></i></a>
+                        <span><?= $likes; ?></span>
+                    </div>
+                    <div class="dislike">
+                        <a href="actions/partnerAction.php?likedislike=2&id=<?= $_GET['partner'] ?>"><i class="fa fa-thumbs-down fa-xl"></i></a>
+                        <span><?= $dislikes; ?></span>
+                    </div>
+                </div>
+                <!-- End like dislike buttons -->
+            </div>
+            <div class="form" id="form">
+                <div class="comment-top">
+                    <div class="title-comment">
+                        <h3 class="form-title">Nouveau commentaire</h3>
+                    </div>
+                    <button id="show" onclick="hide()" class="comment-button">Fermer</button>
+                </div>
+                <?php include 'includes/messageFlash.php'; ?>
+                <form class="sign-form" method="POST">
+                    <div class="field">
+                        <label>Nom</label>
+                        <input class="input" type="text" name="lastname" value="<?= $_SESSION['lastname'];?>" disabled>
+                    </div>
+                    <div class="field">
+                        <label>Texte</label>
+                        <textarea class="input" type="text" name="texte" cols="50" rows="10"></textarea>
+                    </div>
+                    <div class="form-footer">
+                        <button class="btn-comment" type="submit" name="submit">Envoyer</button>
+                    </div> 
+                </form>      
+            </div>
+            <?php foreach ($getCommentsLists as $getCommentsList) : ?>
+                <div class="comment-list">
+                    <div class="comment-name">
+                        <?= $getCommentsList['last_name'];?>
+                    </div>
+                    <div>
+                        <!-- affichage de la date -->
+                        <?= $getCommentsList['created_at_fr'];?>
+                    </div>
+                    <div>
+                        <!-- affichage du commentaire -->
+                        <?= $getCommentsList['texte'];?>
+                    </div>
+                </div>
+            <?php endforeach; ?>                
+        </section>
+    </div>
+    <!-- End comments -->
+    <footer>
+        <div class="mention">
+            <h3>Mentions légales</h3>
+        </div>
+        <div class="barre">
+            <p>|</p>
+        </div>
+        <div class="contact">
+            <h3>Contact</h3>
+        </div>
+    </footer>
+</body>
+</html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script>
+    function show() {
+        document.getElementById('form').style.display="block"
+        document.getElementById('show').style.display="none"
+    }
+    function hide() {
+        document.getElementById('form').style.display="none"
+        document.getElementById('show').style.display="inline"
+    }
+</script>
+
+
+
+
+
